f1f7bf0195bcfafd8fd617126fbe6f4f7260c0ae
enjoy2000 <enjoy3013@gmail.com>
2014-05-06 10:47:15 +0700

    change add job format url key jp

--- app/code/local/Iconic/Job/Model/Job.php
+++ app/code/local/Iconic/Job/Model/Job.php
@@ -11,7 +11,7 @@ class Iconic_Job_Model_Job extends Mage_Core_Model_Abstract
     protected function _beforeSave()
     {
         if(!$this->getUrlKey()){
-            $urlKey = Mage::helper('job')->formatUrlKey($this->getTitle());
+            $urlKey = Mage::helper('job')->formatUrlKeyJp($this->getTitle());
             if(!Mage::getModel('job/job')->load($urlKey, 'url_key')->getId()){
                 $this->setUrlKey($urlKey);
             } else {
