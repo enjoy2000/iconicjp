7c883b66f5a7f5847453cfb9c886bf8e3b71688c
enjoy2000 <enjoy3013@gmail.com>
2014-05-19 15:19:26 +0700

    change error email

--- app/code/local/Iconic/Job/controllers/Customer/AccountController.php
+++ app/code/local/Iconic/Job/controllers/Customer/AccountController.php
@@ -105,7 +105,7 @@ class Iconic_Job_Customer_AccountController extends Mage_Customer_AccountControl
             if ($e->getCode() === Mage_Customer_Model_Customer::EXCEPTION_EMAIL_EXISTS) {
                 $url = Mage::helper('job')->getBaseUrl();
 				Mage::getSingleton('core/session')->setShowForgotPassword(1);
-                $message = $this->__('This email has been use for register. Please click <a href="%s">here</a> to reset password.', $url);
+                $message = $this->__('このメールアドレスは既に登録されています。パスワードをお忘れの場合は、<a href="%s">こちら</a>からパスワードの再発行をしてください。', $url);
                 $session->setEscapeMessages(false);
             } else {
                 $message = $e->getMessage();
