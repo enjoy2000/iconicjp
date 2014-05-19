a673739b2d57304b9751fde680af60b773d57ae1
enjoy2000 <enjoy3013@gmail.com>
2014-05-19 16:22:53 +0700

    add list country model for register page

---
+++ app/code/local/Iconic/Job/Model/Listcountry.php
@@ -0,0 +1,11 @@
+<?php
+ 
+class Iconic_Job_Model_Listcountry extends Mage_Core_Model_Abstract
+{
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init('job/listcountry');
+    }
+
+}
---
+++ app/code/local/Iconic/Job/sql/job_setup/upgrade-0.3.7-0.3.8.php
@@ -0,0 +1,34 @@
+<?php
+ 
+$installer = $this;
+ 
+$installer->startSetup();
+ 
+$installer->run("
+ 
+-- DROP TABLE IF EXISTS {$this->getTable('job/listcountry')};
+CREATE TABLE {$this->getTable('job/listcountry')} (
+  `listcountry_id` int(11) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `name_en` varchar(255) NOT NULL default '',
+  PRIMARY KEY (`listcountry_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('日本','Japan');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('ベトナム','Vietnam');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('インドネシア','Indonesia');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('韓国','Korea');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('中国','China');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('香港','Hong Kong');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('フィリピン','Phillipines');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('ラオス','Laos');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('カンボジア','Cambodia');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('タイ','Thailand');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('マレーシア','Malaysia');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('シンガポール','Singapore');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('ミャンマー','Myanmar');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('バングラデシュ','Bangradesh');
+INSERT INTO {$this->getTable('job/listcountry')} (name,name_en) VALUES ('インド','India');
+    ");
+ 
+$installer->endSetup();
