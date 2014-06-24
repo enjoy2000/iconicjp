b356a5f52e0a532505fb06e38ad5d1553e9e7a3d
enjoy2000 <enjoy3013@gmail.com>
2014-06-24 10:37:13 +0700

    update payload for auto pull from github

---
+++ app/code/local/Iconic/Job/controllers/PayloadCountroller.php
@@ -0,0 +1,13 @@
+<?php
+class Iconic_Job_PayloadController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction(){
+		if ( $_POST['payload'] ) {
+		  	shell_exec( 'cd /var/www/iconicjp/ && git reset --hard HEAD && git pull' );
+			echo 'success';die;
+		}else{
+			$this->_redirect('/');
+			return;
+		}
+	}
+}
