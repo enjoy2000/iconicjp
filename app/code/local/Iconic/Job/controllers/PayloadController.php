e2462f11f1e1773bf046304ffb5af94ee5bebf4e
enjoy2000 <enjoy3013@gmail.com>
2014-07-04 14:56:58 +0700

    test

--- app/code/local/Iconic/Job/controllers/PayloadController.php
+++ app/code/local/Iconic/Job/controllers/PayloadController.php
@@ -14,6 +14,7 @@ class Iconic_Job_PayloadController extends Mage_Core_Controller_Front_Action
 		$commands = array(
 			'echo $PWD',
 			'whoami',
+			'git status',
 			'git pull origin master',
 		);
 	 
