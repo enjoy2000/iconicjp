05995a44fa14caadae73992bc47648e245fad87f
enjoy2000 <enjoy3013@gmail.com>
2014-08-14 16:38:39 +0700

    remove fix logout controller

--- app/code/local/Iconic/Job/controllers/Customer/AccountController.php
+++ app/code/local/Iconic/Job/controllers/Customer/AccountController.php
@@ -301,7 +301,9 @@ class Iconic_Job_Customer_AccountController extends Mage_Customer_AccountControl
      */
     public function logoutAction()
     {
-        $this->_getSession()->logout();
+        $this->_getSession()->logout()
+            ->renewSession()
+            ->setBeforeAuthUrl($this->_getRefererUrl());
 
         header("Location: " . Mage::helper('job')->getBaseUrl());die;
     }
