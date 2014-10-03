c6518c4a20ecf8552b7aba9d27cc1fdfd7eab602
enjoy2000 <enjoy3013@gmail.com>
2014-10-03 10:50:13 +0700

    remove email config and change close btn css

--- app/code/local/Iconic/Job/Helper/Data.php
+++ app/code/local/Iconic/Job/Helper/Data.php
@@ -390,14 +390,7 @@ class Iconic_Job_Helper_Data extends Mage_Core_Helper_Abstract
 	
 	/* Mail config for sending with SMTP */
 	public function getMailConfig(){
-		$config = array(
-	                    'auth' => 'login',
-	                    'ssl'  => 'tls',
-					    'port' =>  587,
-					    'username' => 'info@iconic-jp.com',
-					    'password' => 'bsc393939'
-						);
-		$transport = new Zend_Mail_Transport_Smtp('smtp.gmail.com', $config);
+		$transport = Mage::helper('smtppro')->getTransport();
 		return $transport;
 	}
 }
